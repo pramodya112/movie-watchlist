e4dd4a122530ecea9f60d4dd5f0f37fff9fe635a
pramodya112 <pramodyawarnakula295@gmail.com>
2025-04-09 10:22:26 +0530

    created a movie table

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\ReviewController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Movie routes
+Route::get('/', [MovieController::class, 'index']);
+Route::get('/movie/create', [MovieController::class, 'create']);
+Route::post('/movie', [MovieController::class, 'store']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Review routes
+Route::post('/movie/{movie}/review', [ReviewController::class, 'store']);
+
+require __DIR__.'/auth.php';
